756d19887e92b524afb71bdafd962e58882efafe
scoderr <BahriddinBoboyev997@gmail.com>
2024-02-11 05:11:49 +0500

    config MUI theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+        },
+      },
+    },
+  },
+});
